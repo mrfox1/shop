c77ab8c373fdaa0388e761191dda6eeb4a7470b3
Bohdan <lsdovscky@gmail.com>
2018-04-21 18:26:49 +0300

    add footer

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ Bundler.require(*Rails.groups)
 module Shop
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
+    # application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :sidekiq
   end
